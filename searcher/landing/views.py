9e8897d3238e0f3579927ecea300453232f2ffdc
Muhammad Ihfazhillah <mihfazhillah@gmail.com>
2019-05-29 06:36:58 +0700

    add ability to send email

--- searcher/landing/views.py
+++ searcher/landing/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.views.decorators.http import require_POST
 from scraper.olx import scrape
+from django.core import mail
+from django.template.loader import render_to_string
+from django.utils.html import strip_tags
 
 
 # Create your views here.
@@ -19,3 +22,20 @@ def search(request):
         'result': result
     }
     return render(request, 'search_result.html', context)
+
+
+
+@require_POST
+def send_email(request):
+    query = request.POST.get('query')
+    email = request.POST.get('email')
+    result = scrape(query)
+    context = {
+        'query': query,
+        'result': result
+    }
+    html_message = render_to_string('email.html', context)
+    plain_messages = strip_tags(html_message)
+
+    mail.send_mail(f'result for {query} from ihfazh site', plain_messages, 'From <mihfazhillah@gmail.com>', [email], html_message=html_message)
+    return render(request, 'email_sent.html')
--- searcher/searcher/urls.py
+++ searcher/searcher/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from landing.views import index, search
+from landing.views import index, search, send_email
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
     path('search', search, name='search'),
+    path('send-email', send_email, name='send_email'),
 ]
